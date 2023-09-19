94bc14c5e2647a2c0e52d7c7ed844eb4e8614f44
Ladoxer <ladderdoxer@gmail.com>
2023-09-19 11:10:04 +0530

    finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isLoggedIn: boolean;
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.authService.isLogin();
+  }
+
+  onLoadServers(id: number){
+    // some complex calculation
+    this.router.navigate(['/servers', id, 'edit'], {queryParams:{allowEdit:"1"},fragment:"loading"});
+  }
+
+  onLogin(){
+    this.authService.login();
+    this.isLoggedIn = true;
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.isLoggedIn = false;
   }
 
 }
